0d97bd2d78f8b4632543884d86d17c8c7667f15c
Rogier van Staveren <rogier@batoid.com>
2023-05-27 12:11:45 +0200

    Bump version

--- homeduino/__init__.py
+++ homeduino/__init__.py
@@ -1,11 +1,14 @@
-__version__ = "0.0.7.5"
+__version__ = "0.0.7.6"
 
 from homeduino.homeduino import (
     BAUD_RATES,
     DEFAULT_BAUD_RATE,
     DEFAULT_RECEIVE_PIN,
     DEFAULT_SEND_PIN,
+    DisconnectedError,
     Homeduino,
+    HomeduinoError,
     NotReadyError,
-    ResponseTimeoutError
+    ResponseTimeoutError,
+    TooBusyError,
 )
